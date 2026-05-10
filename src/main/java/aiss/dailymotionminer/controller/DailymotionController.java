8a4db54d7c4c55fa51be41e030b0ff8a2f9a3feb
José Antonio <joscasgar1@alum.us.es>
2026-05-10 22:47:25 +0200

    arreglo de fallos

--- src/main/java/aiss/dailymotionminer/controller/DailymotionController.java
+++ src/main/java/aiss/dailymotionminer/controller/DailymotionController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/dailymotionminer/channels")
+@RequestMapping("/dailymotionminer/channel")
 public class DailymotionController {
 
     @Autowired
