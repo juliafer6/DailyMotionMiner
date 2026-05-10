99761411ec779b53c99fb5ab91129c2167b07a43
José Antonio <joscasgar1@alum.us.es>
2026-05-10 22:40:29 +0200

    arreglado link de DailymotionController.java

--- src/main/java/aiss/dailymotionminer/controller/DailymotionController.java
+++ src/main/java/aiss/dailymotionminer/controller/DailymotionController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/dailymotionminer/channel")
+@RequestMapping("/dailymotionminer/channels")
 public class DailymotionController {
 
     @Autowired
